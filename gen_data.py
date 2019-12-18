272250ece91eb6a7e03ac8cfeda8924553af31f2
bengbmccoy <benjamingbmccoy@gmail.com>
2019-12-18 20:34:35 +1100

    added some extra comments, also changed the classification of outages and clean data from 1 and 0 to clean and outage, so that future classes can be added without confusion

--- gen_data.py
+++ gen_data.py
@@ -62,6 +62,8 @@ def gen_new_data(data_type, val_dict, avg_val_dict, sd_val_dict, days_to_gen):
     # create the new pandas whcih will comtain generated data
     gen_data = pd.DataFrame(columns=list(val_dict.keys()))
 
+    # Randomly add 0kW values to the data, or generate noisy data using the
+    # average and standard deviation.
     if data_type == 'outage':
         for i in range(days_to_gen):
             new_data = {}
@@ -72,11 +74,12 @@ def gen_new_data(data_type, val_dict, avg_val_dict, sd_val_dict, days_to_gen):
                     new_data[key] = float(avg_val_dict[key]) + (float(sd_val_dict[key]) * random.uniform(-1,1))
             gen_data = gen_data.append(new_data, ignore_index=True)
 
+        # Ensure every day has at least one 0kW value
         for i in range(len(gen_data)):
             if 0 not in (gen_data.loc[i,:].values.tolist()):
                 gen_data.iloc[i,random.randint(0,len(gen_data.loc[i,:].values.tolist())-1)] = 0
 
-
+    # Generate noisy data with no outages
     if data_type == 'clean':
         for i in range(days_to_gen):
             new_data = {}
@@ -85,9 +88,9 @@ def gen_new_data(data_type, val_dict, avg_val_dict, sd_val_dict, days_to_gen):
             gen_data = gen_data.append(new_data, ignore_index=True)
 
     if data_type == 'clean':
-        gen_data['class'] = 0
-    else:
-        gen_data['class'] = 1
+        gen_data['class'] = 'clean'
+    elif data_type == 'outage':
+        gen_data['class'] = 'outage'
     return gen_data
 
 def get_sd_val_dict(sd_dict):
