a9fbc4b89193c602ffa7eb8221ec05b4e360a900
bengbmccoy <benjamingbmccoy@gmail.com>
2019-12-29 14:16:55 +1100

    restructured the functions, began work on the data processing required to massage the data to handle only anomalous or non-anomalous data. Still much work to go in structuring the program to take a learning data set, a CV data set and then a test dataset.

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -17,10 +17,28 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('learning_data', type=str,
                         help='example data filepath')
-
+    parser.add_argument('cv_data', type=str,
+                        help='example data filepath')
     args = parser.parse_args()
 
     learning_data = get_data(args.learning_data)
+    print('learning data collected')
+    # print(learning_data)
+
+    feature_df = fill_feature_df(learning_data)
+    print('feature df created and filled')
+    # print(feature_df)
+
+    cv_data = get_data(args.cv_data)
+    print('collected cross validation data')
+    # print(cv_data)
+
+    process_data(cv_data)
+
+def get_data(filename):
+    return pd.read_csv(filename)
+
+def fill_feature_df(learning_data):
     feature_df_labels = (list(learning_data.columns))
     feature_df_labels.pop()
 
@@ -29,11 +47,21 @@ def main():
     for column in feature_df_labels:
         feature_df.loc[column, 'mean'] = learning_data[column].mean()
         feature_df.loc[column, 'SD'] = statistics.stdev(learning_data[column])
+    return feature_df
 
-    print(feature_df)
+def process_data(df):
+    '''
+    takes the data created by gen_data.py and replaces the classes with
+    anomalous or non-anomalous
+    '''
 
-def get_data(filename):
-    return pd.read_csv(filename)
+    for i in range(len(df)):
+        if df.loc[i, 'class'] == 'clean':
+            df.loc[i, 'class'] = '0'
+        else:
+            df.loc[i, 'class'] = '1'
+
+    print(df)
 
 if __name__ == "__main__":
     main()
